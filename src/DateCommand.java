9ece474e7226163787271b0480f2d6d6855a6cdd
Ahmed ElAlfy <ahalfy2005@gmail.com>
2024-10-27 01:46:26 +0300

    adding DateCommand file, v1

---
+++ src/DateCommand.java
@@ -0,0 +1,19 @@
+//package com.shell.command;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateCommand /*implements Command*/ {
+    public static String NAME = "date";
+
+    //@Override
+    public void execute (PrintWriter outputWriter, PrintWriter errorWriter, Scanner inputScanner) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, dd-MM-yyyy HH:mm:ss");
+        String date = now.format(formatter);
+
+        outputWriter.println(date);
+    }
+}
