45a8d85dabc16cb6c5edbebbc56e754e30709a2e
Mahmoud <mahmoudsamymahmoud01@gmail.com>
2024-10-27 13:24:16 +0300

    feat: change types in tokentype

--- src/main/com/shell/TokenType.java
+++ src/main/com/shell/TokenType.java
@@ -1,13 +1,5 @@
 package com.shell;
 
 public enum TokenType {
-    // Commands
-    CLEAR, DATE, ECHO, UNAME, USERS,
-    WHO, PWD, CD, LS, MAN, MKDIR,
-    RMDIR, TOUCH, CP, MV, RM, CAT,
-    MORE, LESS,
-
-    // Operators
-    GREATER, GREATER_GREATER, SMALLER,
-    SMALLER_SMALLER, PIPE
+    COMMAND, FLAG, OPERATOR, PARAMETER
 }
