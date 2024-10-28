e1866fc1547dd9635c9c2de7f3c562c966d5939b
seif <sseif9709@gmail.com>
2024-10-28 23:53:06 +0300

    Feat: add -a -r flags on ls command

--- src/main/com/shell/command/ListCommand.java
+++ src/main/com/shell/command/ListCommand.java
@@ -1,41 +1,67 @@
 package com.shell.command;
 
-
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.io.*;
 import java.util.Scanner;
+import java.util.stream.Stream;
 public class ListCommand implements Command {
     public static final String NAME = "ls";
 
-    private final List<String> directories;
+    private final List<String> arguments;
     private final String workingDirectory;
 
-    public ListCommand(List<String> directories, String workingDirectory) {
-        this.directories = directories;
+    public ListCommand(List<String> arguments, String workingDirectory) {
+        this.arguments = arguments;
         this.workingDirectory = workingDirectory;
     }
 
     @Override
     public void execute(PrintWriter outputWriter, PrintWriter errorWriter, Scanner inputScanner) {
+        // Determine if "-a" or "-r" flags are present
+        boolean showAll = arguments.contains("-a");
+        boolean reverse = arguments.contains("-r");
+
+        // Filter out flags to get directory paths
+        List<String> directories = new ArrayList<>(arguments);
+        directories.removeIf(arg -> arg.equals("-a") || arg.equals("-r"));
+
+        // If no directories are specified, use the working directory
         if (directories.isEmpty()) {
-            listDirectory(workingDirectory, outputWriter, errorWriter);
+            listDirectory(workingDirectory, showAll, reverse, outputWriter, errorWriter);
         } else {
             for (String dirPath : directories) {
                 File dir = new File(workingDirectory, dirPath);
-                listDirectory(dir.getPath(), outputWriter, errorWriter);
+                listDirectory(dir.getPath(), showAll, reverse, outputWriter, errorWriter);
             }
         }
     }
 
-    private void listDirectory(String dirPath, PrintWriter outputWriter, PrintWriter errorWriter) {
+    private void listDirectory(String dirPath, boolean showAll, boolean reverse, PrintWriter outputWriter, PrintWriter errorWriter) {
         File directory = new File(dirPath);
 
         if (directory.isDirectory()) {
+            // Retrieve list of files with filtering and sorting applied
             String[] files = directory.list();
             if (files != null) {
-                for (String file : files) {
-                    outputWriter.println(file);
+                Stream<String> fileStream = Arrays.stream(files);
+
+                // Apply `-a` option: filter hidden files if `showAll` is false
+                if (!showAll) {
+                    fileStream = fileStream.filter(file -> !file.startsWith("."));
                 }
+
+                // Convert stream back to list for sorting if `-r` is true
+                List<String> fileList = new ArrayList<>(fileStream.toList());
+                if (reverse) {
+                    Collections.reverse(fileList);  // Reverse if `-r` option is set
+                }
+
+                // Output each file to outputWriter
+                fileList.forEach(outputWriter::println);
             } else {
                 errorWriter.println(NAME + ": " + dirPath + ": Could not retrieve directory contents");
             }
