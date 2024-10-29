63e06d573a1b9df2539e00ad21071756dcf62a4d
Ahmed ElAlfy <113305116+Ahmed-Aquarius@users.noreply.github.com>
2024-10-29 12:00:37 +0300

    Adding the test file

---
+++ src/DateCommandTest.java
@@ -0,0 +1,49 @@
+//package com.shell.command;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class DateCommandTest {
+    StringWriter outputStringWriter;
+    PrintWriter outputWriter;
+    StringWriter errorStringWriter;
+    PrintWriter errorWriter;
+    Scanner inputScanner;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        outputStringWriter = new StringWriter();
+        outputWriter = new PrintWriter(outputStringWriter);
+        errorStringWriter = new StringWriter();
+        errorWriter = new PrintWriter(errorStringWriter);
+        inputScanner = new Scanner(System.in);
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        outputWriter.close();
+        errorWriter.close();
+        inputScanner.close();
+    }
+
+    @Test
+    void printsCurrentDate_inCorrectFormat() {
+        Command test = new DateCommand();
+        test.execute(outputWriter, errorWriter, inputScanner);
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, dd-MM-yyyy HH:mm:ss");
+        assertThat(outputStringWriter.toString()).isEqualTo(now.format(formatter) + "\r\n");
+        //assertThat(errorStringWriter.toString()).isEmpty();
+    }
+}
